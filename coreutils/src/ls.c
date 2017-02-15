6c90dacff4b45c57bffad6eb8272847966b32289
ogorodnikoff2012 <ogorodnikoff2012@yandex.ru>
2017-02-16 00:05:37 +0300

    Finish '-l' mode (ls, coreutils)

--- coreutils/src/ls.c
+++ coreutils/src/ls.c
@@ -5,14 +5,43 @@
 #include <dirent.h>
 #include <parseargs/parseargs.h>
 #include <string.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
 
-#define RWX_STRING "rwxrwxrwx"
+#define RWX_STRING      "rwxrwxrwx"
+#define TIME_FORMAT     "%b %d %Y %H:%M"
+
+typedef enum {
+    COL_MODE,
+    COL_NLINK,
+    COL_USER,
+    COL_GROUP,
+    COL_FSIZE,
+    COL_CTIME,
+    COL_FNAME,
+    COL_TOTAL_COUNT
+} col_entry_t;
 
 typedef struct dirent dirent_t;
 typedef struct stat stat_t;
+typedef struct timespec timespec_t;
 
 ADD_ARG('l', show_long_info)
 ADD_ARG('a', show_all)
+
+static inline unsigned long long max_u64(unsigned long long a, unsigned long long b) {
+    return a < b ? b : a;
+}
+
+static inline unsigned int int_length(unsigned long long n) {
+    unsigned int ans = 1;
+    while (n >= 10) {
+        ++ans;
+        n /= 10;
+    }
+    return ans;
+}
 
 char filetype_suffix(__mode_t st_mode) {
     switch(st_mode) {
@@ -39,26 +68,66 @@ const char *rwx(unsigned long mode) {
     return str;
 }
 
-void process_direntry(DIR *dir, const dirent_t *entry) {
+const char *get_change_time(const stat_t *stat) {
+    static char str[20];
+    strftime(str, 20, TIME_FORMAT, localtime(&stat->st_ctime));
+    return str;
+}
+
+void prepare_table(DIR *dir, const dirent_t *entry, unsigned int *table) {
     const char *fname = entry->d_name;
-    
+    if (!show_all && fname[0] == '.') { return; }
+
+    stat_t s;
+    fstatat(dirfd(dir), fname, &s, 0);
+    table[COL_NLINK] = max_u64(table[COL_NLINK], int_length(s.st_nlink));
+    table[COL_USER]  = max_u64(table[COL_USER],  strlen(getpwuid(s.st_uid)->pw_name));
+    table[COL_GROUP] = max_u64(table[COL_GROUP], strlen(getgrgid(s.st_gid)->gr_name));
+    table[COL_FSIZE] = max_u64(table[COL_FSIZE], int_length(s.st_size));
+    table[COL_CTIME] = max_u64(table[COL_CTIME], strlen(get_change_time(&s)));
+}
+
+void process_direntry(DIR *dir, const dirent_t *entry, unsigned int *table) {
+    const char *fname = entry->d_name;
     if (!show_all && fname[0] == '.') { return; }
 
     stat_t s;
     fstatat(dirfd(dir), fname, &s, 0);
     if (show_long_info) {
-        printf("%c%s ", 
-                filetype_prefix(s.st_mode & __S_IFMT),
-                rwx(s.st_mode));
+        printf("%c%s %*ld %*s %*s %*ld %*s ", 
+                filetype_prefix(s.st_mode & __S_IFMT), rwx(s.st_mode), 
+                table[COL_NLINK], s.st_nlink,
+                table[COL_USER],  getpwuid(s.st_uid)->pw_name,
+                table[COL_GROUP], getgrgid(s.st_gid)->gr_name,
+                table[COL_FSIZE], s.st_size,
+                table[COL_CTIME], get_change_time(&s));
     }
     printf("%s%c\n", fname, filetype_suffix(s.st_mode & __S_IFMT));
 }
 
 void process_dir(const char *dirname) {
-    DIR *dir = opendir(dirname);
-    dirent_t *entry = readdir(dir);
+    unsigned int table_columns_width[COL_TOTAL_COUNT];
+    for (int i = 0; i < COL_TOTAL_COUNT; ++i) {
+        table_columns_width[i] = 0;
+    }
+
+    DIR *dir;
+    dirent_t *entry;
+
+    if (show_long_info) {
+        dir = opendir(dirname);
+        entry = readdir(dir);
+        while (entry != NULL) {
+            prepare_table(dir, entry, table_columns_width);
+            entry = readdir(dir);
+        } 
+        closedir(dir);
+    }
+
+    dir = opendir(dirname);
+    entry = readdir(dir);
     while (entry != NULL) {
-        process_direntry(dir, entry);
+        process_direntry(dir, entry, table_columns_width);
         entry = readdir(dir);
     } 
     closedir(dir);
